2107497d8cff31835f1afdbe1c51601a02febdac
LUJAINA ALI <lujainaali10@gmail.com>
2025-06-23 03:54:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+         // --------------------------ALERT-----------------------------------//
+
+// 1. Write a script to greet your website visitor using JS alert
+// box.
+// 2. Write a script to display following message on your web
+// page:    
+
+alert("Welcome To My Website!");
+alert("Error!Please enter a valid password.");
+
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+alert("Welcome to JS land...\nHappy Coding!");
+
+
+// 4. Write a script to display following messages in sequence:
+
+// 5. Generate the following message through browser’s
+// developer console:
+
+alert("Hello... I can run JS through my web brower's console.");
